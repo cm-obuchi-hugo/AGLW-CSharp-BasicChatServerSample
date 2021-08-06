a53a774104b7eb18d27d44b6a178c1ec2d4cbd1b
cm-obuchi-hugo <87112231+cm-obuchi-hugo@users.noreply.github.com>
2021-08-06 15:46:58 +0900

    Initial project commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AGLW_CSharp_BasicChatServerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
